49876b24954a59768ee35b1eba59a4796830f103
Fabian <fabianalvarez212@gmail.com>
2022-12-17 18:02:15 -0300

    Menu modificado

--- menu.sh
+++ menu.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-PS3="Elija una opción: "
+#!/usr/bin/env bash
 
 echo "=========================================================="
-
-echo "SCRIPTS PARA PROCESAMIENTO DE TEXTO"
+echo "<<< APP PARA FILTRADO DE TEXTO >>>"
+echo "=========================================================="
 
 TEXTO=$1
-
 read -p "Ingrese el nombre de un archivo de texto: " TEXTO
+echo "=========================================================="
 
 # Verificamos que el archivo fue pasado como argumento
 [ -z $TEXTO ] && echo "Debe ingresar un archivo de texto como argumento" && exit 1
@@ -19,19 +17,34 @@ read -p "Ingrese el nombre de un archivo de texto: " TEXTO
 # Verificamos que el archivo de texto no esté vacío
 [ ! -s $TEXTO ] && echo "El archivo de texto está vacío" && exit 3
 
+
+opcion=0
 echo "=========================================================="
+while [ $opcion -ne 6 ]; do
+	echo
+	echo "¿Qué necesita?"
+	echo "1. La palabra más corta, la más larga y el promedio de longitud de las palabras del texto" 
+	echo "2. Las 10 palabras más usadas que tengan como mínimo 4 letras" 
+	echo "3. Encontrar nombres propios" 
+	echo "4. La oración más corta, la más larga y el promedio de longitud de las oraciones del texto" 
+	echo "5. Contar las líneas en blanco que tiene el texto" 
+	echo "6. SALIR"
+	echo
+	read -p "SELECCIONE UNA OPCIÓN: " opcion
+	echo
+	clear
+        echo "OPCIÓN ELEGIDA:" $opcion
 
-select opcion in "statsWords" "statsUsageWords" "findNames" "statsSentences" "blankLinesCounter" "SALIR"
-do
-	[ -e $opcion ] && echo "Elegir opcion válida!" && continue
-	[ $REPLY == 6 ] && echo "Hasta luego" && break
-
-	echo "Script elegido: " $opcion
-	echo "=========================================================="
-	./${opcion}.sh $TEXTO
+	case $opcion in
+		1) ./statsWords.sh $TEXTO ;;
+		2) ./statsUsageWords.sh $TEXTO ;;
+		3) ./findNames.sh $TEXTO ;;
+		4) ./statsSentences.sh $TEXTO ;;
+		5) ./blankLinesCounter.sh $TEXTO ;;
+		6) echo "Saliendo de la app..."
+		exit 0 ;;
+		*) echo "La opción NO es válida"
+		break ;;
+	esac
 done
-
 echo "=========================================================="
-
-exit 0
-
