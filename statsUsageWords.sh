52a58fd58dc140122f2edab8dea84088599597b2
Fabian <fabianalvarez212@gmail.com>
2022-12-17 20:01:32 -0300

    Listo statsUsageWords

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 function statsUsageWords {
-	cat $1
 	echo "================================================================"
 	echo "Las 10 palabras que más aparecen en el texto son:"
-	cat $1 | iconv -f UTF-8 -t ASCII//TRANSLIT | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr -d '¿' | tr -d '¡' | tr -d '[:digit:]' | tr '[:space:]' '\n' | awk 'length($0) > 3 {print $0}' | sort | uniq -c | sort -g -r | head 
+	cat $1 | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr -d '¿' | tr -d '¡' | tr -d '[:digit:]' | tr '[:space:]' '\n' | awk 'length($0) > 3 {print $0}' | sort | uniq -c | sort -g -r | head 
 	echo "================================================================"
 }
 
